4f97fcb815f55da2bc5d8eead70808bd4eff32c6
OriolGarcia <130761.clot@fje.edu>
2017-01-28 11:42:40 +0100

    20170128 ACTION_ENDED

--- app/src/main/java/com/dam2/android/quickpuzzle/Holders/Listeners/MyDragListener.java
+++ app/src/main/java/com/dam2/android/quickpuzzle/Holders/Listeners/MyDragListener.java
@@ -1,10 +1,15 @@
 package com.dam2.android.quickpuzzle.Holders.Listeners;
 
+import android.content.ClipData;
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.DragEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.dam2.android.quickpuzzle.Holders.PecaHolder;
 
 /**
  * Created by oriol on 28/01/17.
@@ -26,25 +31,39 @@ public class MyDragListener implements View.OnDragListener {
                 break;
             case DragEvent.ACTION_DRAG_EXITED:
                 //  v.setBackgroundDrawable(normalShape);
+               // v.invalidate();
                 break;
             case DragEvent.ACTION_DROP:
+
                 // Dropped, reassign View to ViewGroup
                 //v.setX(event.getX());
-                // v.setY(event.getY());
+
 
                 View view = (View) event.getLocalState();
+                if (false) {
+                    view.setVisibility( View.VISIBLE );
+                    return false;
+                }
                 ViewGroup owner = (ViewGroup) view.getParent();
+
 
                 owner.removeView(view);
                 View view2=new View(view.getContext());;
 
                 owner.addView( view2 );
                 RelativeLayout container = (RelativeLayout) v;
+
                 container.getId();
                 container.addView(view);
                 view.setVisibility(View.VISIBLE);
                 break;
             case DragEvent.ACTION_DRAG_ENDED:
+               view = (View) event.getLocalState();
+                if (!event.getResult()) {
+                    view.setVisibility( View.VISIBLE );
+                    return false;
+
+                }
 
                 //  v.setBackgroundDrawable(normalShape);
             default:
